41bbcdb8f7d39ec3a539d887a4f7edb4c25e26d8
saied <momenzadeh@hamisystem.ir>
2019-09-30 11:37:30 +0330

    initial commit - default empty flutter project

---
+++ android/app/src/main/kotlin/com/saied/hn/saied_hn/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.saied.hn.saied_hn
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
